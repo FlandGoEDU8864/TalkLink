3e5797e8d9114f3bd99165f742b1b7a7c0f6bb1c
Heeeruni <3643781710@qq.com>
2026-06-15 20:41:07 +0800

    Update CmdMessage.py

--- CmdMessage.py
+++ CmdMessage.py
@@ -8,7 +8,7 @@ def versionCommand():
     commandWindow.resizable(False, False)
 
     versionText = Text(commandWindow, height=7, width=41, bg="lightyellow", state=NORMAL, )
-    versionStr = "TalkLink Version 1.0.0\nThank you for using TalkLink software.\nCopyright (C) Fland Studios 2026.\nCrew: Fland_Ho - Coding"
+    versionStr = "TalkLink Version 1.10\nThank you for using TalkLink software.\nCopyright (C) Fland Studios 2026.\nCrew: Fland_Ho - Coding"
     versionText.insert(END, versionStr)
     versionText.config(state=DISABLED)
     versionText.pack()
